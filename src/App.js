bfbd5560fe3459bf37a3693df4b46d0ba3a15e02
LozanoTheStain <adslozanov@gmail.com>
2022-10-06 10:01:22 -0500

    Aprendimos a hacer un componente con imagenes diferentes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import React from 'react'
-// import { Slide } from 'react-slideshow-image'
+import { Slide } from 'react-slideshow-image'
 import 'react-slideshow-image/dist/styles.css'
 import './App.css'
-// import Clock from './components/Clock'
+import Clock from './components/Clock'
 import Tarjet from './components/Tarjet'
+import Welcome from './components/Welcome'
 
-// const images = [
-//   'https://cdn.pixabay.com/photo/2022/09/02/13/35/mountains-7427623_960_720.jpg',
-//   'https://cdn.pixabay.com/photo/2022/09/02/11/27/otter-7427340_960_720.jpg',
-//   'https://cdn.pixabay.com/photo/2014/05/28/20/50/wildcat-356805_960_720.jpg',
-//   'https://cdn.pixabay.com/photo/2022/06/26/14/53/bird-7285669_960_720.jpg',
-//   'https://cdn.pixabay.com/photo/2014/05/28/20/50/wildcat-356805_960_720.jpg',
-// ]
+const images = [
+  'https://cdn.pixabay.com/photo/2022/09/02/13/35/mountains-7427623_960_720.jpg',
+  'https://cdn.pixabay.com/photo/2022/09/02/11/27/otter-7427340_960_720.jpg',
+  'https://cdn.pixabay.com/photo/2014/05/28/20/50/wildcat-356805_960_720.jpg',
+  'https://cdn.pixabay.com/photo/2022/06/26/14/53/bird-7285669_960_720.jpg',
+  'https://cdn.pixabay.com/photo/2014/05/28/20/50/wildcat-356805_960_720.jpg',
+]
 const App = () => {
   return (
     <div>
-      {/* <Slide duration={3000}>
+      <Slide duration={3000}>
         {images.map((image, index) => (
           <div>
             <div key={index} className="each-slide-effect">
@@ -27,15 +28,25 @@ const App = () => {
             </div>
           </div>
         ))}
-      </Slide> */}
-      {/* <Clock /> */}
+      </Slide>
+      <Clock />
       <div className="tarjets">
         <Tarjet />
         <Tarjet />
         <Tarjet />
         <Tarjet />
-        <Tarjet />
       </div>
+      <h1>Buenos días</h1>
+      <Welcome
+        name="Anderson"
+        apellidos="Lozano Velasquez"
+        url="https://cdn.pixabay.com/photo/2015/07/09/22/45/tree-838667_960_720.jpg"
+      />
+      <Welcome
+        name="Pepito"
+        apellidos="Lopera"
+        url="https://cdn.pixabay.com/photo/2016/10/13/00/03/girl-1736419_960_720.jpg"
+      />
     </div>
   )
 }
