dc32363f2cf6fb3f3ab929eee190a2641d5e1387
LozanoTheStain <adslozanov@gmail.com>
2022-10-06 08:30:12 -0500

    Se creo tarjetas de compra, se instalo la libreria bootstrap y se hizo el css pertinente

--- src/components/Tarjet.jsx
+++ src/components/Tarjet.jsx
@@ -3,21 +3,29 @@ import './Tarjet.css'
 
 const Tarjet = () => {
   return (
-    <div className="container-tarjet">
-      <img
-        src="https://cdn.pixabay.com/photo/2014/02/02/17/40/photographs-256888_960_720.jpg"
-        alt=""
-      />
-      <p>
-        De los mejores lentes que se podran encontrar, con esta podras tomar las
-        mejores fotos del mundo
-      </p>
-      <p>⬇ Tiene un precio de ⬇</p>
-      <span>5.000</span>
-      <br />
-      <button>Comprar</button>
-      <br />
-      <button>Añadir al carrito</button>
+    <div className="container">
+      <div className="container-tarjet border">
+        <img
+          className="imagen"
+          src="https://cdn.pixabay.com/photo/2014/02/02/17/40/photographs-256888_960_720.jpg"
+          alt=""
+        />
+        <p>
+          De los mejores lentes que se podran encontrar, con esta podras tomar
+          las mejores fotos del mundo
+        </p>
+        <p>⬇ Tiene un precio de ⬇</p>
+        <br />
+        <span>500.000</span>
+        <br />
+        <button>
+          <i class="fa-solid fa-money-bill"></i> Comprar
+        </button>
+        <br />
+        <button>
+          <i class="fa-solid fa-cart-shopping"></i> Añadir al carrito
+        </button>
+      </div>
     </div>
   )
 }
