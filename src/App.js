36e4f28d8f00f1755f9c110365ff4d1dea7474b8
LozanoTheStain <adslozanov@gmail.com>
2022-10-31 07:04:57 -0500

    Se realizan formularios

--- src/App.js
+++ src/App.js
@@ -1,29 +1,13 @@
 import React from 'react'
-import 'react-slideshow-image/dist/styles.css'
-import './App.css'
-// import Casa from './components/Casa/Casa'
-import Contador from './components/Contador/Contador'
-import Efecto from './components/Efecto'
-import EjemploConStates from './components/EjemploConStates'
-import Pokemon from './components/Pokemon'
-// import Welcome from './welcome/Welcome'
 
 const App = () => {
-  // const usuario = {
-  //   name: 'Anderson',
-  //   lastname: 'Lozano Velasquez',
-  //   avatar:
-  //     'https://i.pinimg.com/564x/c6/f5/40/c6f54056a6e3138c3c063add2f2ecd98.jpg',
-  // }
+  const showAlert = () => {
+    alert('Hola mundo')
+  }
+
   return (
     <div>
-      {/* <Welcome usuario={usuario} />
-      <Casa color="Rojo" tipo="VIS" />
-      <Casa color="Verde" tipo="Palacio" /> */}
-      <Contador />
-      <EjemploConStates />
-      <Efecto />
-      <Pokemon />
+      <button onClick={showAlert}>Mostrar alerta</button>
     </div>
   )
 }
--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -12,7 +12,7 @@ const Pokemon = () => {
 
   useEffect(() => {
     fetchingPoke()
-  }, [])
+  }, /* [] */)
 
   return (
     <div>
