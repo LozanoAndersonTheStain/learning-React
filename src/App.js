6d2961bee775382494d06371c85ba29be6889f7b
LozanoTheStain <adslozanov@gmail.com>
2022-11-08 10:19:01 -0500

    Creando formulario

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import FormularioPost from './components/Formulario-Metodo-Post/FormularioPost'
 // import Formulario from './components/Formulario/Formulario'
 // import Listar from './components/Listar/listar'
 // import FormularioSelect from './components/FormularioSelect/FormularioSelect'
 // import FormilarioStates from './components/FormularioStates/FormilarioStates'
 // import FormularioValidacion from './components/Formulario-Validacion/FormularioValidacion'
-import TablaPersonajes from './components/Consumo-Api/TablaPersonajes'
+// import TablaPersonajes from './components/Consumo-Api/TablaPersonajes'
 
 const App = () => {
   return (
@@ -14,7 +15,8 @@ const App = () => {
       {/* <FormularioSelect /> */}
       {/* <FormilarioStates /> */}
       {/* <FormularioValidacion /> */}
-      <TablaPersonajes />
+      {/* <TablaPersonajes /> */}
+      <FormularioPost />
     </div>
   )
 }
---
+++ src/components/Formulario-Metodo-Post/FormularioPost.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react'
+
+const FormularioPost = () => {
+  const [tittle, setTittle] = useState('')
+  const [date, setDate] = useState('')
+  const [description, setDescription] = useState('')
+  const [author, setAuthor] = useState('')
+  const [image, setImage] = useState('')
+
+  const showAlert = (e) => {
+    e.preventDefault()
+    alert(`${tittle} ${author}`)
+
+    setTittle('')
+    setDate('')
+    setDescription('')
+    setAuthor('')
+    setImage('')
+  }
+
+  return (
+    <div>
+      <form onSubmit={showAlert}>
+        <input
+          type="text"
+          placeholder="Ingrese titulo"
+          value={tittle}
+          onChange={(e) => setTittle(e.target.value)}
+        />
+
+        <input
+          type="date"
+          onChange={(e) => setDate(e.target.value)}
+          value={date}
+        />
+
+        <textarea
+          type="text"
+          placeholder="Ingrese la description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+
+        <input
+          type="text"
+          placeholder="Ingrese el autor de la noticia"
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        />
+
+        <input
+          type="text"
+          placeholder="Ingrese una imagen referente a la noticia"
+          value={image}
+          onChange={(e) => setImage(e.target.value)}
+        />
+
+        <input type="submit" value="enviar" />
+      </form>
+    </div>
+  )
+}
+
+export default FormularioPost
